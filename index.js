b2bd70bf59de76bc4d62f0a6b7383d2b950f4caa
Joseph Frazier <1212jtraceur@gmail.com>
2019-05-08 20:33:10 -0400

    Set up webhook to respond to 'webhook' DMs with a message in a channel

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ controller.configureSlackApp({
 var bot = controller.spawn({
   token: process.env.BOT_TOKEN,
   incoming_webhook: {
-    url: 'WE_WILL_GET_TO_THIS'
+    url: 'https://hooks.slack.com/services/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'
   }
 }).startRTM();
 
@@ -42,4 +42,14 @@ controller.setupWebserver(process.env.PORT, function(err, webserver){
 
 controller.hears('hi', 'direct_message', function(bot, message) {
  bot.reply(message,'Hello.');
+});
+
+controller.hears('webhook', 'direct_message', function(bot, message) {
+  bot.sendWebhook({
+    text: "Hey we've got the webhook!"
+  },function(err,res) {
+    if (err) {
+      console.log('web err', err)
+    }
+  });
 });
